c739f3e6578bdd1f7ee25a435997067a9243d617
fabiojr051003 <fabiojr20032@gmail.com>
2024-09-17 15:47:33 -0300

    Commit Atualizado3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-import os
-import uvicorn
 from fastapi import FastAPI
 from app.routers import products
 from sqlmodel import SQLModel, create_engine
@@ -14,10 +12,3 @@ SQLModel.metadata.create_all(engine)
 
 # Inclusão das rotas
 app.include_router(products.router, prefix="/api", tags=["produtos"])
-@app.get("/")
-def read_root():
-    return {"Api Geek"}
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Usa a variável de ambiente PORT ou 8000 por padrão
-    uvicorn.run(app, host="0.0.0.0", port=port)
